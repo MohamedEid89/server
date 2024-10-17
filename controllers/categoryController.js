7959d23612f44de76ee385bbee24fffba57e83fe
Mohammed Eid <77382624+MohamedEid89@users.noreply.github.com>
2024-10-17 02:57:47 +0200

    installation and categories
    Add, remove ,update ,get categories

---
+++ controllers/categoryController.js
@@ -0,0 +1,61 @@
+const { default: slugify } = require("slugify");
+const Category = require("../models/CategoryModel");
+const expressAsyncHandler = require("express-async-handler");
+
+// @dec Get Categories
+// @route GET /api/v1/categories 
+// @access Public
+exports.getCategories = expressAsyncHandler(async (req, res) => {
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 10;
+    const skip = (page - 1) * limit;
+    const categories = await Category.find({}).skip(skip).limit(limit);
+    res.status(200).json({ result: categories.length, page, data: categories });
+})
+
+// @dec Get specific category by id
+// @route GET /api/v1/categories/:id
+// @access Public
+exports.getCategory = expressAsyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const category = await Category.findById(id);
+    if (!category) {
+        res.status(404).json({ msg: `No category found for this id ${id}` });
+    }
+    res.status(200).json({ data: category });
+})
+
+
+// @dec Create Category
+// @route POST /api/v1/categories 
+// @access Private
+exports.createCategory = expressAsyncHandler(async (req, res) => {
+    const name = req.body.name;
+    const category = await Category.create({ name, slug: slugify(name) });
+    res.status(201).json({ data: category })
+});
+
+// @dec Update specific category 
+// @route POST /api/v1/categories/:id 
+// @access Private
+exports.updateCategory = expressAsyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    const category = await Category.findByIdAndUpdate({ _id: id }, { name, slug: slugify(name) }, { new: true });
+    if (!category) {
+        res.status(404).json({ msg: `No category found for this id ${id}` });
+    }
+    res.status(200).json({ data: category });
+})
+
+// @dec Delete specific category 
+// @route POST /api/v1/categories/:id
+// @access Private
+exports.deleteCategory = expressAsyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const category = await Category.findByIdAndDelete(id);
+    if (!category) {
+        res.status(404).json({ msg: `No category found for this id ${id}` });
+    }
+    res.status(204).send();
+})
